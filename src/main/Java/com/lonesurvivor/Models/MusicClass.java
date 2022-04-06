3e0432262db93a37973edbdb03dc4ee05217b429
Percell Gaskins <pmg8899@gmail.com>
2022-04-06 14:43:59 -0700

    AB#426,AB#528,"GUICodeBase"

--- src/main/Java/com/lonesurvivor/Models/MusicClass.java
+++ src/main/Java/com/lonesurvivor/Models/MusicClass.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class MusicClass {
     public void audioFile() {
         try {
-            File song = new File("src/main/resources/Get Up OffaThat Thing.wav");
+            File song  =  new File("src/main/resources/Get Up OffaThat Thing.wav");
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(song);
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
