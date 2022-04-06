04812d28d3c2eda73ed4e2626be60fe482c04f9f
Clayvis Rawlins <Clayvis27@gmail.com>
2022-04-06 12:32:49 -0700

    player class refactor v1, AB#427

--- src/main/Java/com/lonesurvivor/GameEngine/GameEngine.java
+++ src/main/Java/com/lonesurvivor/GameEngine/GameEngine.java
@@ -30,9 +30,9 @@ public class GameEngine {
     private List<String> command;
     private String input;
     private Location playerLocation;
-    private int dayCount;
     private double randNum;
     private boolean hasWon;
+    public static int dayCount;
 
 
     public GameEngine() throws IOException, ParseException {
@@ -47,8 +47,8 @@ public class GameEngine {
         //jsonParser = new JSONParser();
         //reader = new FileReader("src/Java/External_Files/location.json");
         //file = (JSONArray) jsonParser.parse(reader);
-        player = new Player();
-        dayCount = 1;
+        player = new Player("John Doe", locations);
+        setDayCount(1);
         //randNum = Math.random();
         hasWon = false; //better name could be hasWon
     }
@@ -106,10 +106,10 @@ public class GameEngine {
          */
         System.out.println("\n******************************************************");
         System.out.println("It is Day " + dayCount);
-        System.out.println("You are currently located in " + playerLocation.getName().toUpperCase());
-        System.out.println(playerLocation.getDescription());
+        System.out.println("You are currently located in " + player.getPlayerLocation().getName().toUpperCase());
+        System.out.println(player.getPlayerLocation().getName());
         //System.out.println("Items: " + playerLocation.getItems());
-        System.out.println("Directions: " + playerLocation.getDirection());
+        System.out.println("Directions: " + player.getPlayerLocation().getDirection());
 
         /* NOTE: INPUT PROMPT
             -prompts user for input
@@ -127,6 +127,36 @@ public class GameEngine {
     }
 
     public void commandProcessor() throws IOException, ParseException {
+
+        //converted if statements to a switch. And moved commands to player class
+        //move engine
+        switch (command.get(0)) {
+            case "go":
+                player.moveEngine(command.get(1));
+                break;
+            case "get":
+                //get engine
+                player.getEngine(command.get(1));
+                break;
+            case "look":
+                //look engine
+                player.lookEngine(command.get(1));
+                break;
+            case "use":
+                //Use engine
+                //still need to implement
+                break;
+            case "quit":
+                //quit engine
+                player.quitEngine(command.get(1));
+                break;
+            case "help":
+                //help engine
+                player.helpEngine(command.get(1));
+                break;
+        }
+
+        /*
         if (command.get(0).equals("go")){
             //move engine
             moveEngine(command.get(1));
@@ -151,158 +181,167 @@ public class GameEngine {
             //help engine
             helpEngine(command.get(1));
         }
+        */
     }
 
-    private void moveEngine(String noun){
+//    private void moveEngine(String noun){
+//
+//        Map<String, String> playerDirection = playerLocation.getDirection();
+//        String choice = playerDirection.get(noun);
+//        randNum = Math.random();
+//
+//        try {
+//            for (int i = 0; i < locations.size(); i++) {
+//                if (choice.equals("???")){
+//                    //randNum = Math.random();
+//                    if(randNum < 0.33){
+//                        //choice = "mysterious animal"
+//                        //choice = "forest dead end"
+//                        player.setPlayerLocation(locations.get(9));
+//                    }
+//                    else if (randNum >= 0.33 && randNum <= 0.66){
+//                        player.setPlayerLocation(locations.get(10));
+//                        //dayCount++;
+//                    }
+//                    else {
+//                        player.setPlayerLocation(locations.get(6));
+//                    }
+//                }
+//                else if (choice.equals("cross bridge") && noun.equals("east")) {
+//                    player.setPlayerLocation(locations.get(7));
+//                }
+//                else if (choice.equals("cross bridge") && noun.equals("west")) {
+//                    player.setPlayerLocation(locations.get(6));
+//                }
+//                else if (choice.equals("investigate sound")) {
+//                    //randNum = Math.random();
+//                    if (randNum < 0.5){
+//                        player.setPlayerLocation(locations.get(8));
+//                    }
+//                    else {
+//                        player.setPlayerLocation(locations.get(9));
+//                        //dayCount++;
+//                    }
+//                    //player.setPlayerLocation(locations.get(8));
+//
+//                }
+//                if (choice.equals(locations.get(i).getName())) {
+//                    player.setPlayerLocation(locations.get(i));
+//                }
+//            }
+//            playerLocation = player.getPlayerLocation();
+//            if (playerLocation.getName().equals("lair - mysterious animal") || playerLocation.getName().equals("forest dead end")) {
+//                dayCount++;
+//            }
+//        }
+//        catch (NullPointerException e) {
+//            System.out.println("Invalid command!");
+//        }
+//
+//        /*playerLocation = player.getPlayerLocation();
+//        if (playerLocation.getName().equals("lair - mysterious animal") || playerLocation.getName().equals("forest dead end")) {
+//            dayCount++;
+//        }*/
+//    }
 
-        Map<String, String> playerDirection = playerLocation.getDirection();
-        String choice = playerDirection.get(noun);
-        randNum = Math.random();
-        
-        try {
-            for (int i = 0; i < locations.size(); i++) {
-                if (choice.equals("???")){
-                    //randNum = Math.random();
-                    if(randNum < 0.33){
-                        //choice = "mysterious animal"
-                        //choice = "forest dead end"
-                        player.setPlayerLocation(locations.get(9));
-                    }
-                    else if (randNum >= 0.33 && randNum <= 0.66){
-                        player.setPlayerLocation(locations.get(10));
-                        //dayCount++;
-                    }
-                    else {
-                        player.setPlayerLocation(locations.get(6));
-                    }
-                }
-                else if (choice.equals("cross bridge") && noun.equals("east")) {
-                    player.setPlayerLocation(locations.get(7));
-                }
-                else if (choice.equals("cross bridge") && noun.equals("west")) {
-                    player.setPlayerLocation(locations.get(6));
-                }
-                else if (choice.equals("investigate sound")) {
-                    //randNum = Math.random();
-                    if (randNum < 0.5){
-                        player.setPlayerLocation(locations.get(8));
-                    }
-                    else {
-                        player.setPlayerLocation(locations.get(9));
-                        //dayCount++;
-                    }
-                    //player.setPlayerLocation(locations.get(8));
+//    private void getEngine(String noun) throws IOException, ParseException {
+//        //get item from JSON file
+//        //reader = new FileReader("src/Java/External_Files/location.json");
+//        //JSONArray fileArray = (JSONArray) jsonParser.parse(reader);
+//
+//        //JSONObject inventory = (JSONObject) fileArray.get(0); //this line needs to be based on where the player is
+//        //JSONObject item = (JSONObject) inventory.get("inventory");
+//        //String itemName = (String) inventory.get("locationItems");
+//
+//        //String itemName = (String) item.get("itemName");
+//        //System.out.println(itemName);
+//        if (noun.equals("inventory")) {
+//            System.out.println(player.getItems());
+//        }
+//        List<String> playerItem = playerLocation.getItems();
+//
+//        if (playerItem.contains(noun)){
+//            player.addItems(noun);
+//            //need to remove items from location after picking it up
+//            for (int i = 0; i < locations.size(); i++) {
+//                if (locations.get(i).getItems().contains(noun)){
+//                    locations.get(i).getItems().remove(noun);
+//                    System.out.println(noun + " was removed from " + locations.get(i).getName());
+//                }
+//            }
+//
+//        }
+//        else {
+//            System.out.println("There is no " + noun);
+//        }
+//
+//    }
+//
+//    private void lookEngine(String noun) throws IOException, ParseException {
+//        //get location from JSON file
+//        //based on players location, will determine where player can go, what they can do
+//        //You look around and see to the north: first class, south: service area
+//        //you see the following items: flashlight, life jacket
+//
+//
+//        System.out.println("You look around and see: ");
+//        System.out.println(playerLocation.getDescription());
+//        System.out.println("Items: " + playerLocation.getItems());
+//        System.out.println("Directions: " + playerLocation.getDirection());
+//
+//        /*for (Location location : locations) {
+//            //JSONObject location = (JSONObject) fileArray.get(0);
+//            //display location name, location description, then directions, then items that can be picked up
+//
+//            //String location = (String) locationName.get("locationName");
+//            //System.out.println(obj);
+//            //JSONObject location = (JSONObject) obj;
+//
+//            //JSONArray locItems = (JSONArray) location.get("locationItems");
+//            //JSONArray locDirections = (JSONArray) location.get("locationDirections");
+//
+//            System.out.println(location.getName());
+//            System.out.println(location.getDescription());
+//            System.out.println(location.getItems());
+//            System.out.println(location.getDirection());
+//        }*/
+//
+//
+//
+//
+//    }
+//
+//    private void useEngine(String noun){}
+//
+//    private void quitEngine(String noun){
+//        if (noun.equals("game")) {
+//            System.out.println("Quitting now...");
+//            System.exit(0);
+//        }
+//    }
+//
+//    private void helpEngine(String noun) throws IOException, ParseException {
+//        if (noun.equals("commands")) {
+//            //reader = new FileReader("src/Java/External_Files/CommandList.json");
+//            //JSONArray fileInfo = (JSONArray) jsonParser.parse(reader);
+//            //JSONObject gameInfo = (JSONObject) fileInfo.get(2);
+//            //System.out.println(gameInfo);
+//            System.out.println(jsonParserClass.commandParser().get(2));
+//        }
+//        else if (noun.equals("game")){
+//            //reader = new FileReader("src/Java/External_Files/GameInfo.json");
+//            //JSONObject fileInfo = (JSONObject) jsonParser.parse(reader);
+//            //String gameInfo = (String) fileInfo.get("gameInfo");
+//            //System.out.println(gameInfo);
+//            System.out.println(jsonParserClass.gameInfoParser());
+//        }
+//    }
 
-                }
-                if (choice.equals(locations.get(i).getName())) {
-                    player.setPlayerLocation(locations.get(i));
-                }
-            }
-            playerLocation = player.getPlayerLocation();
-            if (playerLocation.getName().equals("lair - mysterious animal") || playerLocation.getName().equals("forest dead end")) {
-                dayCount++;
-            }
-        }
-        catch (NullPointerException e) {
-            System.out.println("Invalid command!");
-        }
-
-        /*playerLocation = player.getPlayerLocation();
-        if (playerLocation.getName().equals("lair - mysterious animal") || playerLocation.getName().equals("forest dead end")) {
-            dayCount++;
-        }*/
+    public int getDayCount() {
+        return dayCount;
     }
 
-    private void getEngine(String noun) throws IOException, ParseException {
-        //get item from JSON file
-        //reader = new FileReader("src/Java/External_Files/location.json");
-        //JSONArray fileArray = (JSONArray) jsonParser.parse(reader);
-
-        //JSONObject inventory = (JSONObject) fileArray.get(0); //this line needs to be based on where the player is
-        //JSONObject item = (JSONObject) inventory.get("inventory");
-        //String itemName = (String) inventory.get("locationItems");
-
-        //String itemName = (String) item.get("itemName");
-        //System.out.println(itemName);
-        if (noun.equals("inventory")) {
-            System.out.println(player.getItems());
-        }
-        List<String> playerItem = playerLocation.getItems();
-
-        if (playerItem.contains(noun)){
-            player.addItems(noun);
-            //need to remove items from location after picking it up
-            for (int i = 0; i < locations.size(); i++) {
-                if (locations.get(i).getItems().contains(noun)){
-                    locations.get(i).getItems().remove(noun);
-                    System.out.println(noun + " was removed from " + locations.get(i).getName());
-                }
-            }
-
-        }
-        else {
-            System.out.println("There is no " + noun);
-        }
-
-    }
-
-    private void lookEngine(String noun) throws IOException, ParseException {
-        //get location from JSON file
-        //based on players location, will determine where player can go, what they can do
-        //You look around and see to the north: first class, south: service area
-        //you see the following items: flashlight, life jacket
-
-
-        System.out.println("You look around and see: ");
-        System.out.println(playerLocation.getDescription());
-        System.out.println("Items: " + playerLocation.getItems());
-        System.out.println("Directions: " + playerLocation.getDirection());
-
-        /*for (Location location : locations) {
-            //JSONObject location = (JSONObject) fileArray.get(0);
-            //display location name, location description, then directions, then items that can be picked up
-
-            //String location = (String) locationName.get("locationName");
-            //System.out.println(obj);
-            //JSONObject location = (JSONObject) obj;
-
-            //JSONArray locItems = (JSONArray) location.get("locationItems");
-            //JSONArray locDirections = (JSONArray) location.get("locationDirections");
-
-            System.out.println(location.getName());
-            System.out.println(location.getDescription());
-            System.out.println(location.getItems());
-            System.out.println(location.getDirection());
-        }*/
-
-
-
-
-    }
-
-    private void useEngine(String noun){}
-
-    private void quitEngine(String noun){
-        if (noun.equals("game")) {
-            System.out.println("Quitting now...");
-            System.exit(0);
-        }
-    }
-
-    private void helpEngine(String noun) throws IOException, ParseException {
-        if (noun.equals("commands")) {
-            //reader = new FileReader("src/Java/External_Files/CommandList.json");
-            //JSONArray fileInfo = (JSONArray) jsonParser.parse(reader);
-            //JSONObject gameInfo = (JSONObject) fileInfo.get(2);
-            //System.out.println(gameInfo);
-            System.out.println(jsonParserClass.commandParser().get(2));
-        }
-        else if (noun.equals("game")){
-            //reader = new FileReader("src/Java/External_Files/GameInfo.json");
-            //JSONObject fileInfo = (JSONObject) jsonParser.parse(reader);
-            //String gameInfo = (String) fileInfo.get("gameInfo");
-            //System.out.println(gameInfo);
-            System.out.println(jsonParserClass.gameInfoParser());
-        }
+    public void setDayCount(int dayCount) {
+        this.dayCount = dayCount;
     }
 }
--- src/main/Java/com/lonesurvivor/Models/Player.java
+++ src/main/Java/com/lonesurvivor/Models/Player.java
@@ -1,24 +1,190 @@
 package com.lonesurvivor.Models;
 
-import java.util.ArrayList;
+import com.lonesurvivor.GameEngine.GameEngine;
+import com.lonesurvivor.Utils.JSONParserClass;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
 import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+
 
 public class Player {
 
+    // Fields
     private String name;
-    //private ArrayList<String> inventory;
-    private Set<String> inventory;
+    private Set<String> inventory = new LinkedHashSet<>();
     private Location playerLocation;
+    private List<Location> locations;
+    private JSONParserClass jsonParserClass = new JSONParserClass();
 
-    public Player() {
-        inventory = new LinkedHashSet<>();
+
+    public Player(String name, List<Location> locations) throws IOException, ParseException {
+        this.name = name;
+        this.locations = locations;
+
+
+    // instantiate when class is instantiated
+    // inventory = new LinkedHashSet<>();
     }
 
-    /*public Player(String name, ArrayList<String> items) {
-        this.name = name;
-        this.inventory = items;
-    }*/
+    // Business Methods
+
+    public void moveEngine(String noun){
+
+        Map<String, String> playerDirection = playerLocation.getDirection();
+        String choice = playerDirection.get(noun);
+        double randNum = Math.random();
+
+        try {
+            for (Location location : locations) {
+                if (choice.equals("???")) {
+                    //randNum = Math.random();
+                    if (randNum < 0.33) {
+                        //choice = "mysterious animal"
+                        //choice = "forest dead end"
+                        setPlayerLocation(locations.get(9));
+                    } else if (randNum >= 0.33 && randNum <= 0.66) {
+                        setPlayerLocation(locations.get(10));
+                        //dayCount++;
+                    } else {
+                        setPlayerLocation(locations.get(6));
+                    }
+                } else if (choice.equals("cross bridge") && noun.equals("east")) {
+                    setPlayerLocation(locations.get(7));
+                } else if (choice.equals("cross bridge") && noun.equals("west")) {
+                    setPlayerLocation(locations.get(6));
+                } else if (choice.equals("investigate sound")) {
+                    //randNum = Math.random();
+                    if (randNum < 0.5) {
+                        setPlayerLocation(locations.get(8));
+                    } else {
+                        setPlayerLocation(locations.get(9));
+                        //dayCount++;
+                    }
+                    //player.setPlayerLocation(locations.get(8));
+
+                }
+                if (choice.equals(location.getName())) {
+                    setPlayerLocation(location);
+                }
+            }
+            playerLocation = getPlayerLocation();
+            if (playerLocation.getName().equals("lair - mysterious animal") || playerLocation.getName().equals("forest dead end")) {
+                GameEngine.dayCount++;
+            }
+        }
+        catch (NullPointerException e) {
+            System.out.println("Invalid command!");
+        }
+
+        /*playerLocation = player.getPlayerLocation();
+        if (playerLocation.getName().equals("lair - mysterious animal") || playerLocation.getName().equals("forest dead end")) {
+            dayCount++;
+        }*/
+    }
+
+    public void getEngine(String noun) throws IOException, ParseException {
+        //get item from JSON file
+        //reader = new FileReader("src/Java/External_Files/location.json");
+        //JSONArray fileArray = (JSONArray) jsonParser.parse(reader);
+
+        //JSONObject inventory = (JSONObject) fileArray.get(0); //this line needs to be based on where the player is
+        //JSONObject item = (JSONObject) inventory.get("inventory");
+        //String itemName = (String) inventory.get("locationItems");
+
+        //String itemName = (String) item.get("itemName");
+        //System.out.println(itemName);
+        if (noun.equals("inventory")) {
+            System.out.println(getItems());
+        }
+        List<String> playerItem = playerLocation.getItems();
+
+        if (playerItem.contains(noun)){
+            addItems(noun);
+            //need to remove items from location after picking it up
+            for (Location location : locations) {
+                if (location.getItems().contains(noun)) {
+                    location.getItems().remove(noun);
+                    System.out.println(noun + " was removed from " + location.getName());
+                }
+            }
+
+        }
+        else {
+            System.out.println("There is no " + noun);
+        }
+
+    }
+
+    public void lookEngine(String noun) throws IOException, ParseException {
+        //get location from JSON file
+        //based on players location, will determine where player can go, what they can do
+        //You look around and see to the north: first class, south: service area
+        //you see the following items: flashlight, life jacket
+
+
+        System.out.println("You look around and see: ");
+        System.out.println(playerLocation.getDescription());
+        System.out.println("Items: " + playerLocation.getItems());
+        System.out.println("Directions: " + playerLocation.getDirection());
+
+        /*for (Location location : locations) {
+            //JSONObject location = (JSONObject) fileArray.get(0);
+            //display location name, location description, then directions, then items that can be picked up
+
+            //String location = (String) locationName.get("locationName");
+            //System.out.println(obj);
+            //JSONObject location = (JSONObject) obj;
+
+            //JSONArray locItems = (JSONArray) location.get("locationItems");
+            //JSONArray locDirections = (JSONArray) location.get("locationDirections");
+
+            System.out.println(location.getName());
+            System.out.println(location.getDescription());
+            System.out.println(location.getItems());
+            System.out.println(location.getDirection());
+        }*/
+
+
+    }
+
+    public void useEngine(String noun){}
+
+    public void quitEngine(String noun){
+        if (noun.equals("game")) {
+            System.out.println("Quitting now...");
+            System.exit(0);
+        }
+    }
+
+    public void helpEngine(String noun) throws IOException, ParseException {
+        if (noun.equals("commands")) {
+            //reader = new FileReader("src/Java/External_Files/CommandList.json");
+            //JSONArray fileInfo = (JSONArray) jsonParser.parse(reader);
+            //JSONObject gameInfo = (JSONObject) fileInfo.get(2);
+            //System.out.println(gameInfo);
+            System.out.println(jsonParserClass.commandParser().get(2));
+        }
+        else if (noun.equals("game")){
+            //reader = new FileReader("src/Java/External_Files/GameInfo.json");
+            //JSONObject fileInfo = (JSONObject) jsonParser.parse(reader);
+            //String gameInfo = (String) fileInfo.get("gameInfo");
+            //System.out.println(gameInfo);
+            System.out.println(jsonParserClass.gameInfoParser());
+        }
+    }
+
+
+
+    public void addItems(String item) {
+        inventory.add(item);
+        System.out.println("Player has added " + item + " to their inventory. ");
+    }
+
+    // Accessor Methods
 
     public String getName() {
         return name;
@@ -29,11 +195,6 @@ public class Player {
         return inventory;
     }
 
-    public void addItems(String item) {
-        inventory.add(item);
-        System.out.println("Player has added " + item + " to their inventory. ");
-    }
-
     public void setPlayerLocation(Location location) {
         playerLocation = location;
     }
@@ -42,6 +203,7 @@ public class Player {
         return playerLocation;
     }
 
-    public void performAction() {}
+//    // empty method
+//    public void performAction() {}
 
 }
