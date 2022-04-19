475bad0655a000c9f1105c2ef7575e114f29dd54
David Ledesma <davidled97@gmail.com>
2022-04-18 19:46:17 -0700

    use radio, AB#391

--- src/main/Java/com/lonesurvivor/Items/Radio.java
+++ src/main/Java/com/lonesurvivor/Items/Radio.java
@@ -4,19 +4,23 @@ import com.lonesurvivor.Models.Location;
 import com.lonesurvivor.Models.Player;
 import com.lonesurvivor.Views.LocationFrame;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Radio extends Item{
+    Timer timer = new Timer();
+    public static Clip clip = null;
 
     public void turnOn() throws InterruptedException {
         Location playerLocation = Player.getInstance().getPlayerLocation();
         ArrayList<String> items = Player.getInstance().getInventory();
         for (int aItem = 0; aItem < items.size(); aItem++) {
             if (items.get(aItem).equals("radio")) {
-                LocationFrame.textDisplayGui("This radio looks like it can still work! I am going to turn it on and hopefully it works....IT DOES WORK!");
-                Timer timer = new Timer();
+                LocationFrame.textDisplayGui("This radio looks like it can still work! I am going to try to turn it on....IT DOES WORK!");
                 timer.schedule(new TimerTask() {
                     public void run() {
                         playMusic();
@@ -29,6 +33,48 @@ public class Radio extends Item{
     }
 
     public void playMusic(){
-        LocationFrame.textDisplayGui("*Playing Music*");
+        AudioInputStream audioStream = null;
+        File getFx = new File("src/main/resources/soundFX/radio-transmission.wav");
+        try {
+            audioStream = AudioSystem.getAudioInputStream(getFx);
+            clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+            timer.schedule(new TimerTask() {
+                public void run(){
+                    sendMessage();
+                }
+            },3000);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void sendMessage(){
+        LocationFrame.textDisplayGui("You: If someone is out there please send some help! I was in flight 210, my name is Rennie");
+        timer.schedule(new TimerTask() {
+            public void run(){
+                stopAudio();
+            }
+        },8000);
+    }
+
+    public void stopAudio(){
+        clip.stop();
+        timer.schedule(new TimerTask() {
+            public void run(){
+                rescued();
+            }
+        },10000);
+    }
+
+    public void rescued(){
+        LocationFrame.textDisplayGui("Wait a second...the call worked! There is a team here to rescue me!");
+        try{
+            Thread.sleep(5000);
+        }catch(InterruptedException e){
+        e.printStackTrace();
+        }
+        System.exit(0);
     }
 }
